e6a7ca227e72a6ddf50ae42b1f775c878c1871e7
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-07-21 23:29:16 +0545

    Update Experiences.jsx

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -9,13 +9,6 @@ export default function Experiences() {
     
 
     {
-      id: 4,
-      title: 'White Water Rafting',
-      image: 'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=800',
-      description: 'Plunge down wild, glacial rivers fed directly from snowy peaks on thrilling rafting excursions.',
-      tag: 'Adrenaline',
-    },
-    {
       id: 5,
       title: 'Tandem Paragliding',
       image: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=800',
