0034a5c8386d42e9579d29852f358daef43252ec
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-08-19 19:39:44 +0545

    Remove main headline from Hero component
    Removed the main headline from the Hero component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,14 +54,7 @@ export default function Hero() {
             </motion.div>
 
             {/* Main Headline */}
-            <motion.h1
-              variants={itemVariants}
-              className="font-display font-extrabold text-4xl sm:text-5xl lg:text-7xl text-white leading-tight"
-            >
-              Your Dream <br />
            
-              Starts Here
-            </motion.h1>
 
             {/* Subheadline */}
          
